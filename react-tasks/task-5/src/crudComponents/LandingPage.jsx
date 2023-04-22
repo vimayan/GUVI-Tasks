1a5e87fe8519375afe89831ac852421cbf7fa7dd
vimayan <joblessdesks@gmail.com>
2023-04-22 07:57:20 +0530

    update

--- react-tasks/task-5/src/crudComponents/LandingPage.jsx
+++ react-tasks/task-5/src/crudComponents/LandingPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 function LandingPage() {
     return ( <>
 <div className='container-fluid' id='landing_page'>
