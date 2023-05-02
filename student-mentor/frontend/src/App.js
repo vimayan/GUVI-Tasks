b748d4882a2bdee3f2717095e207e6d0cbbb9f77
vimayan <joblessdesks@gmail.com>
2023-05-02 22:36:44 +0530

    Update App.js

--- student-mentor/frontend/src/App.js
+++ student-mentor/frontend/src/App.js
@@ -1,30 +1,25 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from './page/LandingPage';
-import HomePage from './page/HomePage';
-import StudentPage from './page/StudentPage';
-import MentorPage from './page/MentorPage';
-import SelectedStudentPage from './page/SelectedStudent';
-import SelectedMentorPage from './page/SelectedMentor';
-
+import LandingPage from "./page/LandingPage";
+import HomePage from "./page/HomePage";
+import StudentPage from "./page/StudentPage";
+import MentorPage from "./page/MentorPage";
+import SelectedStudentPage from "./page/SelectedStudent";
+import SelectedMentorPage from "./page/SelectedMentor";
 
 function App() {
   return (
     <div className="App">
-      
-<BrowserRouter>
-<Routes>
-<Route path='/' element={<LandingPage/>}/>
-<Route path='/'>
-  <Route path='home' element={<HomePage/>}/>
-  <Route path='mentor' element={<MentorPage/>}/>
-  <Route path='student' element={<StudentPage/>}/>
-  <Route path='student/:student' element={<SelectedStudentPage/>}/>
-  <Route path='mentor/:mentor' element={<SelectedMentorPage/>}/>
-</Route>
-
-</Routes>
-</BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/mentor" element={<MentorPage />} />
+          <Route path="/student" element={<StudentPage />} />
+          <Route path="/student/:student" element={<SelectedStudentPage />} />
+          <Route path="/mentor/:mentor" element={<SelectedMentorPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
