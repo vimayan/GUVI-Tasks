a9808d728f35002aa7902d2a7f649ce705440451
vimayan <joblessdesks@gmail.com>
2023-05-08 22:11:49 +0530

    Update Login.jsx

--- nodemailer/client/src/page/Login.jsx
+++ nodemailer/client/src/page/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React,{ useState } from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
