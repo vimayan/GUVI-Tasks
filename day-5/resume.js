89f2e33e47b85deb75cecad370d22e174c1149a8
vimayan <joblessdesks@gmail.com>
2023-06-01 12:29:53 +0530

    Create resume.js

---
+++ day-5/resume.js
@@ -0,0 +1,77 @@
+const resume= {
+    "name": "Villavan Murugan",
+    "title": "MERN Stack Developer",
+    "github": "github.com/vimayan",
+    "linkedin": "linkedin.com/in/villavan-eng2016",
+    "email": "joblessdesks@gmail.com",
+    "phone": "+91-9952254654",
+    "about": "Somehow learning JavaScript surprises me more than my experience as a product designer. I have been learning JavaScript with the knowledge I had and working on coding tutorials, debugging, and DOM manipulation. I have endeavored to explore new technologies and keen to utilize my skills.",
+    "education": [
+      {
+        "degree": "BE in Manufacturing Engineering",
+        "university": "CIPET",
+        "start_date": "2012",
+        "end_date": "2016",
+        "percentage": "64.5%"
+      },
+      {
+        "Higher Secondary": " Government Higher Secondary School ",
+        "start_date": "2010",
+        "end_date": "2012",
+        "percentage": "92%"
+      }
+    ],
+    "skills": [
+      "Front End",
+      "EcmaScript-6",
+      "HTML-5",
+      "CSS-3",
+      "React JS",
+      "Redux",
+      "Formik",
+      "Bootstrap-5",
+      "Material-UI",
+      "Back End",
+      "Node JS",
+      "Express JS",
+      "Mongoose",
+      "Socket IO",
+      "Nodemailer",
+      "Joi",
+      "Restful APIs",
+      "Database",
+      "MongoDB",
+      "Version Control",
+      "Git",
+      "GitHub",
+      "Other Tools",
+      "Postman",
+      "Netlify",
+      "Render",
+      "AWS"
+    ],
+    "certifications": [
+      {
+        "title": "MERN Stack Developer",
+        "year": "2023",
+        "issuer": "GUVI"
+      }
+    ],
+    "projects": [
+      {
+        "title": "Ticket Booking",
+        "description": "A ticket booking application built using MERN stack.",
+        "link": "https://guvi-hackathon2-frontend-villavan.netlify.app"
+      },
+      {
+        "title": "React JS Deployment",
+        "description": "A website built using React JS and deployed on Netlify.",
+        "link": "https://villavan-react-training.netlify.app"
+      },
+      {
+        "title": "Fitness Logger",
+        "description": "A fitness logging application built using MERN stack.",
+        "link": "https://fitness-loggerl-villavan.netlify.app"
+      }
+    ]
+  }
